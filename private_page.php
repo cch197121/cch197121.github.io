c9abf6287867039872b21f92e68070e8204cd7df
cch197121 <132177892+cch197121@users.noreply.github.com>
2023-08-02 10:12:31 +0800

    Update private_page.php

--- private_page.php
+++ private_page.php
@@ -1,33 +1,16 @@
-<?php
-session_start();
-
-// 检查用户是否已经登录，如果未登录，则重定向到登录页面
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: index.php");
-    exit();
-}
-?>
-
 <!DOCTYPE html>
-<html>
-<head>
-    <title>mine的网站 - 私密内容</title>
-    <!-- 添加所需的CSS样式 -->
-</head>
-<body>
-    <header>
-        <h1>欢迎来到mine的网站 - 私密空间</h1>
-    </header>
-    <main>
-        <section>
-            <h2>私密内容</h2>
-            <p>这里是您的私密内容。</p>
-            <a href="index.php?logout=true">注销</a>
-        </section>
-    </main>
-    <footer>
-        <p>版权所有 ©2022 公司名称</p>
-    </footer>
-    <!-- 添加所需的JavaScript脚本 -->
-</body>
-</html>
+<html> _ _
+<头>
+    < title >放弃空间</ title >
+    <元字符集= “UTF-8” >
+    <链接rel = “样式表”  href = “style.css” >
+</头>
+<正文>
+    < h1 >退出空间</ h1 >
+    <p>这是公开的内容，只有经过验证的用户才能看到。</p>
+    <!-- 在这里添加您感兴趣的内容 -->
+    <页脚>
+        < p >版权所有 ©2022 公司名称</ p >
+    </页脚>
+</正文>
+</html> _ _
