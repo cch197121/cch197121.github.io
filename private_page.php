9d736248df09a64b0ca92844022b7a5a4130971b
cch197121 <132177892+cch197121@users.noreply.github.com>
2023-08-02 08:41:18 +0800

    Update private_page.php

--- private_page.php
+++ private_page.php
@@ -8,17 +8,40 @@
 </head>
 <body>
     <h1>私密页面</h1>
-    <?php
-    session_start();
-    if(isset($_SESSION['username'])) {
-        $username = $_SESSION['username'];
-        echo "<p>欢迎，$username！这是私密内容。</p>";
-        // 显示其他私密内容
-        // ...
-    } else {
-        header('Location: login.php'); // 如果用户未登录，则重定向到登录页面
-        exit();
-    }
-    ?>
+    
+    <!-- 密码和身份验证的表单 -->
+    <div id="authentication-form">
+        <h2>请输入密码和身份验证信息</h2>
+        <form onsubmit="authenticate(event)">
+            <input type="text" id="username" placeholder="用户名" required>
+            <input type="password" id="password" placeholder="密码" required>
+            <button type="submit">验证</button>
+        </form>
+    </div>
+    
+    <!-- 其他私密内容 -->
+    <p>这是私密页面的其他内容。</p>
+    
+    <script>
+        function authenticate(event) {
+            event.preventDefault();
+            
+            var usernameInput = document.getElementById("username");
+            var passwordInput = document.getElementById("password");
+            
+            // 获取用户输入的用户名和密码
+            var username = usernameInput.value;
+            var password = passwordInput.value;
+            
+            // 验证用户名和密码是否与管理员的登录信息匹配
+            if (username === "管理员" && password === "mine3314") {
+                // 验证通过，允许访问私密内容
+                
+            } else {
+                // 验证失败，显示错误消息或重定向到其他页面
+                
+            }
+        }
+    </script>
 </body>
 </html>
