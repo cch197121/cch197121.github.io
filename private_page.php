b1623a1e60217ee22888c6098163ce4d4892ae61
cch197121 <132177892+cch197121@users.noreply.github.com>
2023-08-02 09:39:45 +0800

    Update private_page.php

--- private_page.php
+++ private_page.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-// 检查用户是否已登录，如果未登录，则重定向到登录页面
+// 检查用户是否已经登录，如果未登录，则重定向到登录页面
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 ?>
@@ -12,11 +12,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 <html>
 <head>
     <title>mine的网站 - 私密内容</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <style>
-        /* 自定义样式 */
-    </style>
+    <!-- 添加所需的CSS样式 -->
 </head>
 <body>
     <header>
@@ -26,12 +22,12 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         <section>
             <h2>私密内容</h2>
             <p>这里是您的私密内容。</p>
-            <a href="logout.php">注销</a>
+            <a href="index.php?logout=true">注销</a>
         </section>
     </main>
     <footer>
         <p>版权所有 ©2022 公司名称</p>
     </footer>
-    <script src="script.js"></script>
+    <!-- 添加所需的JavaScript脚本 -->
 </body>
 </html>
